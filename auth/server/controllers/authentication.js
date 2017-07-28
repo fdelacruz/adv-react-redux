e32ffd70236a62245ce4621fcadd68c3e97de344
Francisco De La Cruz <helloquico@gmail.com>
2017-07-27 21:45:56 -0400

    Search for Users

--- auth/server/controllers/authentication.js
+++ auth/server/controllers/authentication.js
@@ -1,3 +1,29 @@
+const User = require('../models/user');
+
 exports.signup = function(req, res, next) {
-  res.send({ sucess: 'true' });
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // See if an user with the given email exists
+  User.findOne({ email: email }, function(err, existingUser) {
+    if (err) { return next(err); }  
+
+    // If an user with email does exist, return an error
+    if (existingUser) {
+      return res.status(422).send({ error: 'Email is in use'});
+    }
+
+    // If an user with email does NOT exist, create and save user record
+    const user = new User({
+      email: email,
+      password: password
+    });
+
+    user.save(function(err) {
+      if (err) { return next(err); }
+
+      // Respond to request indicating the user was created
+      res.json({ success: true });
+    });
+  });
 }
