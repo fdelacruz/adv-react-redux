edd7d1beea2e1957357a69acff95fbde6fef7fa6
Francisco De La Cruz <helloquico@gmail.com>
2017-07-30 08:15:45 -0400

    Action Creator with Many Responsabilities

---
+++ client/src/actions/index.js
@@ -0,0 +1,11 @@
+export function sininUser({ email, password }) {
+  // Submit email/password to the server
+
+  // If request is good:
+  // - Update state to indicate user is authenticated
+  // - Save the JWT token
+  // - Redirect to the route '/feature'
+
+  // if request is BAD:
+  // - Show an error to the user
+}
